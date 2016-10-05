0303095e487b19d834c69e263b21c38747e1055c
ctchervenkov <ctchervenkov@gmail.com>
2016-10-05 17:37:50 +0200

    added crude local maxima finder for image normalization

--- python/itti_saliency.py
+++ python/itti_saliency.py
@@ -8,7 +8,7 @@ def getIntensityImage(raw):
 	I = (b+g+r)/3.0;
 	return I, r, g, b;
 
-def getColorChannels(r,b,g,I):
+def getColorChannels(b,g,r,I):
 	I_max = np.amax(I);
 	for i in range(I.shape[0]):
 		for j in range(I.shape[1]):
@@ -53,7 +53,7 @@ def buildGaborPyramid(src,octaves,thetas):
 	gp = np.ndarray(shape=(thetas.size,1),dtype=object);
 	for t in range(thetas.size):
 		theta = thetas[t];
-		kernel = cv2.getGaborKernel((9,9), 1, theta, 1, 1, 0);
+		kernel = cv2.getGaborKernel((5,5), 1, theta, 1, 1, 0);
 		kernel = kernel/np.sum(kernel);
 		gp[t,0] = buildPyramid(src,kernel,octaves);
 
@@ -75,26 +75,66 @@ def centerSurroundDiff(im_c,im_s):
 	im_cs = np.absolute(im_c - im_s);
 	return im_cs;
 
+def findLocalMax(src):
+	dst = np.zeros(src.shape);
+	locMax = np.zeros((1,src.size));
+	locMaxInd = np.ndarray(shape=(1,src.size),dtype=object);
+	it = 0;
+
+	for iy in range(1,src.shape[0]-1):
+		for ix in range(1,src.shape[1]-1):
+			if ((((src[iy,ix] > src[iy-1,ix]) and (src[iy,ix] > src[iy,ix-1])) and (src[iy,ix] > src[iy+1,ix])) and (src[iy,ix] > src[iy,ix+1])):
+				dst[iy,ix] = True;
+				locMax[0,it] = src[iy,ix];
+				locMaxInd[0,it] = (iy,ix);
+				it += 1;
+
+	locMax = locMax[0,0:it];
+	locMaxInd = locMaxInd[0,0:it];
+
+	return dst, locMax, locMaxInd
+
 def mapNormalization(src):
 	if np.absolute(np.amax(src)) > 0.0:
 		src = src / np.amax(src) * 100.0;
 	minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(src);
 	M = maxVal;
 	threshold = 0.5*(maxVal - minVal) + minVal;
-	m = 0;
-	nb_peaks = 0;
 
-	while maxVal > threshold:
-		maxVal = np.amax(src[src<maxVal]);
-		m += maxVal;
-		nb_peaks += 1;
+	dst, locMax, locMaxInd = findLocalMax(src);
 
-	if nb_peaks > 0:
-		m /= nb_peaks;
-	# print M, m
+	locMaxInRange = locMax[np.logical_and(locMax<M,locMax>threshold)];
+	m = 0.0;
+	if locMaxInRange.size > 0:
+		m = np.true_divide(np.sum(locMaxInRange),locMaxInRange.size);
+
+	# m = 0.0;
+	# nb_peaks = 0.0;
+	# for iy in range(1,src.shape[0]-1):
+	# 	for ix in range(1,src.shape[1]-1):
+	# 		if ((src[iy,ix] < M) and (src[iy,ix] > threshold)):
+	# 			if ((((src[iy,ix] > src[iy-1,ix]) and (src[iy,ix] > src[iy,ix-1])) and (src[iy,ix] > src[iy+1,ix])) and (src[iy,ix] > src[iy,ix+1])):
+	# 				m += src[iy,ix];
+	# 				nb_peaks += 1.0;
+	# if nb_peaks > 0:
+	# 	m /= nb_peaks;
+	
+	# threshold = 0.5*(maxVal - minVal) + minVal;
+	# m = 0;
+	# nb_peaks = 0;
+
+	# while maxVal > threshold:
+	# 	maxVal = np.amax(src[src<maxVal]);
+	# 	m += maxVal;
+	# 	nb_peaks += 1;
+
+	# if nb_peaks > 0:
+	# 	m /= nb_peaks;
+
 	out = src * np.power(M-m,2);
-
 	return out;
+
+
 
 def buildIntensityConspicuityMap(I_pyr,c_list,delta_list):
 	reduce_scale = np.amax(c_list);
@@ -154,17 +194,17 @@ plt.subplot(1,5,2),plt.imshow(r, cmap='gray'), plt.title('r')
 plt.subplot(1,5,3),plt.imshow(g, cmap='gray'), plt.title('g')
 plt.subplot(1,5,4),plt.imshow(b, cmap='gray'), plt.title('b')
 plt.subplot(1,5,5),plt.imshow(I, cmap='gray'), plt.title('I')
-# plt.show()
+
 
 # Normalize r,g,b by I & create color channels
-R, G, B, Y = getColorChannels(r,b,g,I);
+R, G, B, Y = getColorChannels(b,g,r,I);
 
 plt.figure()
 plt.subplot(1,4,1),plt.imshow(R, cmap='gray'), plt.title('R')
 plt.subplot(1,4,2),plt.imshow(G, cmap='gray'), plt.title('G')
 plt.subplot(1,4,3),plt.imshow(B, cmap='gray'), plt.title('B')
 plt.subplot(1,4,4),plt.imshow(Y, cmap='gray'), plt.title('Y')
-# plt.show()
+
 
 # Build Gaussian pyramids
 I_gp = buildGaussianPyramid(I,nb_octaves);
@@ -183,17 +223,17 @@ I_bar = buildIntensityConspicuityMap(I_gp[0,0],np.array([2,3,4]),np.array([3,4])
 C_bar = buildColorConspicuityMap(R_gp[0,0],G_gp[0,0],B_gp[0,0],Y_gp[0,0],np.array([2,3,4]),np.array([3,4]))
 O_bar = buildOrientationConspicuityMap(O_gp,np.array([2,3,4]),np.array([3,4]))
 
+# plt.figure()
+# plt.subplot(1,2,1),plt.imshow(I_bar, cmap='gray'), plt.title('I')
+# plt.subplot(1,2,2),plt.imshow(max_I, cmap='gray'), plt.title('I maxima')
+
 plt.figure()
 plt.subplot(1,3,1),plt.imshow(I_bar, cmap='gray'), plt.title('Conspicuity Map Intensity')
 plt.subplot(1,3,2),plt.imshow(C_bar, cmap='gray'), plt.title('Conspicuity Map Color')
 plt.subplot(1,3,3),plt.imshow(O_bar, cmap='gray'), plt.title('Conspicuity Map Orientation')
 
-# plt.show();
-# print I_bar.shape, C_bar.shape
-
-#TODO: Build orientation conspicuity map
-O_bar = np.zeros(I_bar.shape);
-S = (I_bar + C_bar + O_bar)/3.0;
+# Saliency
+S = (mapNormalization(I_bar) + mapNormalization(C_bar) + mapNormalization(O_bar))/3.0;
 
 
 plt.figure()
